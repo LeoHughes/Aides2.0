157438419e99e83cf9dc2d7d3365d3e1b088ae7b
zhanghuan <zhihy123@qq.com>
2017-05-24 15:46:57 +0800

    update

--- mod/reg.js
+++ mod/reg.js
@@ -1,6 +1,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var date_reg = /^(?:(?!0000)[0-9]{4}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[0-9]{2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[2468][048]|[13579][26])00)-02-29)$/;
 var email_reg = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)/;
+var phone_reg = /(^[1][3][0-9]{9}$)|(^[1][4][0-9]{9}$)|(^[1][5][0-9]{9}$)|(^[1][7][0-9]{9}$)|(^[1][8][0-9]{9}$)/;
 var IP_reg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])((\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])){3}|(\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])){5})$/;
 var verifyAccount_reg = /^[a-zA-Z][a-zA-Z0-9_]{5,14}$/;
 var IDcard_reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
@@ -68,6 +69,9 @@ var Reg = (function () {
     };
     Reg.prototype.isDate = function (time) {
         return this._testRE(date_reg, time);
+    };
+    Reg.prototype.isPhone = function (phone) {
+        return this._testRE(phone_reg, phone);
     };
     Reg.prototype.isEmail = function (email) {
         return this._testRE(email_reg, email);
