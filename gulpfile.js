cf0133bccb2307e9cc4831c1ff61fbca9bb5b44d
LeoHughes <zhihy123@qq.com>
2017-06-15 09:47:09 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var tsProject = ts.createProject("tsconfig.json");
 /**
  * 分别编译各个工具包
  */
-gulp.task("ts", function () {
+gulp.task("ts", function() {
   return tsProject.src()
     .pipe(tsProject(tsProject))
     .pipe(gulp.dest("mod"));
@@ -20,28 +20,27 @@ gulp.task("ts", function () {
 /**
  * 统一打包编译aidesWin js文件
  */
-gulp.task("webpack", function () {
-
+gulp.task("webpack", function() {
   return webpack({
-    entry: "./src/aidesWin.ts",
-    resolve: {
-      extensions: ['', '.ts']
-    },
-    output: {
-      filename: 'aidesWin.js'
-    },
-    module: {
-      loaders: [
-        { test: /\.ts$/, loader: 'ts-loader' }
-      ]
-    }
-  })
+      entry: "./src/aidesWin.ts",
+      resolve: {
+        extensions: ['', '.ts']
+      },
+      output: {
+        filename: 'aidesWin.js'
+      },
+      module: {
+        loaders: [
+          { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+      }
+    })
     .pipe(gulp.dest("dist"));
 })
 
 
 //编译为es5
-gulp.task("es5", ["webpack"], function () {
+gulp.task("es5", ["webpack"], function() {
   return gulp.src(["./dist/aidesWin.js"])
     .pipe(babel({
       presets: ["es2015"]
@@ -51,8 +50,8 @@ gulp.task("es5", ["webpack"], function () {
       rename('aidesWin.min.js')
     )
     .pipe(
-        uglify()
-     )             
+      uglify()
+    )
     .pipe(gulp.dest("dist"));
 })
 
@@ -60,9 +59,9 @@ gulp.task("es5", ["webpack"], function () {
 /**
  * 默认任务
  */
-gulp.task("default", ["es5","ts"], function () {
+gulp.task("default", ["es5", "ts"], function() {
 
   //  监控js文件
-  gulp.watch("./src/*.ts", ["es5","ts"]);
+  gulp.watch("./src/*.ts", ["es5", "ts"]);
 
 });
